12521651b593b99ef8bfe909f8d34b0e9e40c978
Nicolas Cwiluk <ncwiluk@gmail.com>
2023-06-20 11:59:53 -0300

    creacion de componente cartwidget

---
+++ src/components/common/cartWidget/CartWidget.jsx
@@ -0,0 +1,11 @@
+import { BsFillCartCheckFill } from "react-icons/bs";
+
+const CartWidget = () => {
+  return (
+    <div>
+      <BsFillCartCheckFill color="white" size="40px" />
+    </div>
+  );
+};
+
+export default CartWidget;
